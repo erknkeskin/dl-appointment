524604a174a12fddab958bb0d5f71ec33ead01e4
erknkeskin <keskin.erkan@gmail.com>
2021-05-06 00:34:13 +0300

    config edit

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
+import socket
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://erkan:123@localhost/appointmentdb1'
+    if socket.gethostname() == 'erkankeskin-pc':
+        SQLALCHEMY_DATABASE_URI = 'postgresql://erkan:123@localhost/appointmentdb1'
+    else:
+        SQLALCHEMY_DATABASE_URI = 'postgresql://admin_appousr:1lq3PZlnmR@localhost/admin_appointmentdb1'
+
     SQLALCHEMY_TRACK_MODIFICATION = False
